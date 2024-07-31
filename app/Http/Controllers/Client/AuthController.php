ab4ba844527c834179ded309df71c7158b306027
iqbalwaldan <iqbalwaldan.social@gmail.com>
2024-08-01 05:57:34 +0700

    Cookies Fix

--- app/Http/Controllers/Client/AuthController.php
+++ app/Http/Controllers/Client/AuthController.php
@@ -25,11 +25,11 @@ class AuthController extends Controller
         }
         return view('client.user.auth.login.index');
     }
-
+    
     public function login(Request $request)
     {
         setcookie('facebookData', '', time() + (86400 * 30), "/");
-
+        
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
@@ -48,9 +48,6 @@ class AuthController extends Controller
 
             // Save the session token in a cookie
             setcookie('tokenlogin', $sessionToken, time() + (86400 * 30), "/"); // Cookie will expire in 30 days
-
-            // Set cookie
-            setcookie('facebookData', DashboardController::fetchFacebookData(), time() + (86400 * 30), "/"); // Cookie will expire in 30 days
 
             return redirect()->intended('/dashboard');
         }
