29c530a0b28bf8aaaa852170f542a8e825aee010
iqbalwaldan <iqbalwaldan.social@gmail.com>
2024-08-01 05:18:05 +0700

    Set Cookies

--- app/Http/Controllers/Client/AuthController.php
+++ app/Http/Controllers/Client/AuthController.php
@@ -46,10 +46,9 @@ class AuthController extends Controller
             // Save the session token in a cookie
             setcookie('tokenlogin', $sessionToken, time() + (86400 * 30), "/"); // Cookie will expire in 30 days
 
-            if (!isset($_COOKIE['facebookData'])) {
-                // Set cookie
-                setcookie('facebookData', DashboardController::fetchFacebookData(), time() + (86400 * 30), "/"); // Cookie will expire in 30 days
-            }
+            // Set cookie
+            setcookie('facebookData', DashboardController::fetchFacebookData(), time() + (86400 * 30), "/"); // Cookie will expire in 30 days
+
             return redirect()->intended('/dashboard');
         }
         // return back()->with('error', 'Login failed!');
